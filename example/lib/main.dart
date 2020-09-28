e18df1762ec92e48436c93825a5a13e8f6488a40
yozoon <yozoon.dev@gmail.com>
2020-09-28 11:48:10 +0200

    speed_dial is now scrollable when space is limited. Added quick action widget space.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -45,9 +45,12 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
     return SpeedDial(
       animatedIcon: AnimatedIcons.menu_close,
       animatedIconTheme: IconThemeData(size: 22.0),
-      // child: Icon(Icons.add),
+      child: Icon(Icons.add),
       onOpen: () => print('OPENING DIAL'),
       onClose: () => print('DIAL CLOSED'),
+      quickActionWidget: QuickActionBox(
+        actionTitles: ['Action', 'Action with longer text', 'Quick', 'Go!'],
+      ),
       visible: dialVisible,
       curve: Curves.bounceIn,
       children: [
@@ -68,15 +71,20 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
           labelBackgroundColor: Colors.green,
         ),
         SpeedDialChild(
-          child: Icon(Icons.keyboard_voice, color: Colors.white),
-          backgroundColor: Colors.blue,
-          onTap: () => print('THIRD CHILD'),
-          labelWidget: Container(
-            color: Colors.blue,
-            margin: EdgeInsets.only(right: 10),
-            padding: EdgeInsets.all(6),
-            child: Text('Custom Label Widget'),
-          ),
+          child: Icon(Icons.accessibility, color: Colors.white),
+          backgroundColor: Colors.deepOrange,
+          onTap: () => print('FIRST CHILD'),
+          label: 'First Child',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.deepOrangeAccent,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.brush, color: Colors.white),
+          backgroundColor: Colors.green,
+          onTap: () => print('SECOND CHILD'),
+          label: 'Second Child',
+          labelStyle: TextStyle(fontWeight: FontWeight.w500),
+          labelBackgroundColor: Colors.green,
         ),
       ],
     );
@@ -88,6 +96,91 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
       appBar: AppBar(title: Text('Flutter Speed Dial')),
       body: buildBody(),
       floatingActionButton: buildSpeedDial(),
+    );
+  }
+}
+
+class QuickActionBox extends StatelessWidget {
+  const QuickActionBox({Key key, @required this.actionTitles})
+      : super(key: key);
+
+  final List<String> actionTitles;
+
+  @override
+  Widget build(BuildContext context) {
+    final quickActions = List<Widget>.from(actionTitles.map(
+      (title) => QuickActionButton(title: title),
+    ));
+
+    return Container(
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width - 32,
+      child: Builder(
+        builder: (context) {
+          if (MediaQuery.of(context).size.height / 3 < 3 * 48) {
+            return SingleChildScrollView(
+              clipBehavior: Clip.none,
+              scrollDirection: Axis.horizontal,
+              child: Row(children: quickActions),
+            );
+          }
+          return Wrap(
+            clipBehavior: Clip.none,
+            runAlignment: WrapAlignment.spaceEvenly,
+            alignment: WrapAlignment.center,
+            direction: Axis.horizontal,
+            children: quickActions,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class QuickActionButton extends StatelessWidget {
+  const QuickActionButton({Key key, @required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: MaterialButton(
+        height: 48,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.0),
+            side: BorderSide(color: Theme.of(context).accentColor)),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Attention'),
+                content: Text('Proceed with "$title"?'),
+                actions: [
+                  FlatButton(
+                    child: Text('CANCEL'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('YES'),
+                    onPressed: () {
+                      print(title);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        textTheme: ButtonTextTheme.accent,
+        child: Text(title),
+        color: Theme.of(context).scaffoldBackgroundColor,
+      ),
     );
   }
 }
